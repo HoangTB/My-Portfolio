1c552a196a60b3e546773a30ae5800b21802a21e
HoangTB <tbhoang95@gmail.com>
2023-10-14 14:35:51 +0700

    'create'

---
+++ src/content_option.js
@@ -0,0 +1,121 @@
+const logotext = "https://res.cloudinary.com/dh6r4v7ei/image/upload/v1697253788/HoangTB/1_hbbzih.png";
+const meta = {
+    title: "Truong Bao Hoang",
+    description: "I'm Truong Bao Hoang, a web developer.",
+};
+
+const introdata = {
+    title: "Hi, I'm TRUONG BAO HOANG",
+    animated: {
+        first: "Web Developer",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Without requirements or design, programming is the art of adding bugs to an empty text file. -Louis Srygley-",
+    your_img_url: "https://res.cloudinary.com/dh6r4v7ei/image/upload/v1697255376/HoangTB/238700722_1700117533515987_1150959040732583828_n_rnapeb.jpg",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "I love solving problems and enjoy learning! Currently, I'm a intern at RikkeiSoft Danang for full-stack developer. I have expereince with HTML5, CSS3, Javascript, ReactJS, NodeJS (ExpressJS and NestJS).",
+};
+const worktimeline = [{
+        jobtitle: "Web Developer",
+        where: "RIKKEI SOFT",
+        date: "20239-2023 - Present",
+    },
+    {
+        jobtitle: "Internships Trainee",
+        where: "Work in Japan",
+        date: "2020 - 2023",
+    },
+    {
+        jobtitle: "IT Technician",
+        where: "FSOFT DA NANG - DPS",
+        date: "2017 - 2019",
+    },
+];
+
+const skills = [{
+        name: "HTML5",
+    },
+    
+    {
+        name: "CSS3",
+    },
+    
+    {
+        name: "Java Scripts",
+    },
+    
+    {
+        name: "ReactJS",
+    },
+
+    {
+        name: "Bootstrap",
+    },
+
+    {
+        name: "TypeScripts",
+    },
+
+    {
+        name: "NodeJS",
+    },
+
+    {
+        name: "NestJS",
+    },
+
+     {
+        name: "MySQL",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://res.cloudinary.com/dh6r4v7ei/image/upload/v1697257943/HoangTB/Screenshot_2023-10-14_111646_bd8nyc.png",
+        description: "Ecomerce Web Jayden Shoes use HTML5, CSS3, JavaScript, Bootstrap",
+        link: "https://jayden-b4end4dm1-tbhoang95-gmailcom.vercel.app/",
+    },
+    {
+        img: "https://res.cloudinary.com/dh6r4v7ei/image/upload/v1697257884/HoangTB/Screenshot_2023-10-14_112946_djdtxl.png",
+        description: "Mygram Social Network use HTML5, CSS3, JavaScript, Bootstrap, Localstorage, Json Server",
+        link: "https://github.com/HoangTB/Mygram",
+    },
+    {
+        img: "https://res.cloudinary.com/dh6r4v7ei/image/upload/v1697257883/HoangTB/Screenshot_2023-10-14_113046_kxjggw.png",
+        description: "Ecomerce Web Nike Fullstack With ReactJS, NodeJS(ExpressJS), MySQL",
+        link: "https://nike-type-scripts-q593.vercel.app/",
+    },
+    {
+        img: "https://res.cloudinary.com/dh6r4v7ei/image/upload/v1697257883/HoangTB/Screenshot_2023-10-14_113046_kxjggw.png",
+        description: "Ecomerce Web Nike Fullstack With ReactJS, NodeJS(NestJS) and MySQL",
+        link: "https://github.com/HoangTB/Nike-NestJS",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "tbhoang95@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_hfz5pk8",
+    YOUR_TEMPLATE_ID: "template_y0x8sbb",
+    YOUR_USER_ID: "loVQleHppaRDXt6cr",
+};
+
+const socialprofils = {
+    github: "https://github.com/HoangTB",
+    facebook: "https://www.facebook.com/tbhoang95/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
